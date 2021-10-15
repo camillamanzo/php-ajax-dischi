358e6bd569383e09dd7164b5e0dda7c1a023be3f
camillamanzo <camillasouthafrica@gmail.com>
2021-10-15 17:12:04 +0200

    printed title and author

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php include __DIR__ . '/./db/database.php'; ?>
-    
-</body>
-</html>
+<?php include __DIR__ . '/./db/database.php'; ?>
+
+<?php foreach($newJson as $arr){ ?>
+
+    <?php foreach($arr as $arr2){ ?>
+
+        <img :src="<?php echo ($arr2["poster"]); ?>" :alt="record img">
+        <h3><?php echo $arr2["title"]; ?></h3>
+        <p><?php echo $arr2["author"]; ?></p>
+
+    <?php }; ?>
+
+<?php }; ?>
+
